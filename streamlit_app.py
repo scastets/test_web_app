2e6fe56fc1a241f9ebea638981bf5d83e5ec48a0
Sébastien Castets <78219214+scastets@users.noreply.github.com>
2025-02-11 14:59:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.express as px
 
-# Titre du dashboard
-st.title("Dashboard interactif avec Streamlit")
+def main():
+    st.title("Détection de l'extinction de l'éclairage public")
+    
+    src = 'radiance_time_series_interp_9_q90.parquet'
+    df = pd.read_parquet(src, index_col=0)
 
-# Création d'un slider pour modifier la fréquence de la sinusoïde
-freq = st.slider("Choisissez une fréquence", min_value=1, max_value=20, value=5)
+    commune = st.selectbox("Choisir une commune", df['code_insee'].unique())
+    
+    df_rad_values = df[df['code_insee'] == commune].drop(columns=['code_insee'])
+        
+    print(df_rad_values)
 
-# Génération des données
-x = np.linspace(0, 10, 100)
-y = np.sin(freq * x)
-
-# Affichage du graphique
-fig, ax = plt.subplots()
-ax.plot(x, y, label=f"Sinus {freq} Hz")
-ax.legend()
-st.pyplot(fig)
+if __name__ == "__main__":
+    main()
